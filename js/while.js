4912f67c45e204f430b62b7d2ea5266c4d9e0351
Ignacio Mares <ignacio.mares00@gmail.com>
2021-07-15 09:59:39 -0500

    updated cones solution

--- js/while.js
+++ js/while.js
@@ -5,16 +5,23 @@
 // }
 
 var allCones = Math.floor(Math.random() * 50) + 50;
-alert('You have ' + allCones + ' cones to start with.')
+// alert('You have ' + allCones + ' cones to start with.')
 do {
     var howManyCones = Math.floor(Math.random() * 5) + 1;
-    var totalConesLeft = allCones - howManyCones;
-    allCones = allCones - howManyCones
-    alert('You would like to buy ' + howManyCones);
-    alert('We have ' + totalConesLeft + ' cones left to sell.');
+    if (howManyCones > allCones){
+        console.log("Cannot sell you" + howManyCones + " I only have " + allCones);
+    } else {
+        allCones = allCones - howManyCones;
+        console.log(howManyCones + "cones sold..." + allCones + "cones left");
+    }
+    // console.log(howManyCones + 'cones sold...')
+    // var totalConesLeft = allCones - howManyCones;
+
+    // alert('You would like to buy ' + howManyCones);
+    // alert('We have ' + totalConesLeft + ' cones left to sell.');
 
 }
-while (totalConesLeft >= 1){
-    alert('We are all out of cones, Sorry!')
+while (allCones > 0){
+    console.log('Yay! I sold them all!');
 
 };
