e04e171e64c1970055a91e2e5043b6362be2293c
Ignacio Mares <ignacio.mares00@gmail.com>
2021-07-15 17:30:42 -0500

    updated loops problems

---
+++ js/for_loops.js
@@ -0,0 +1,24 @@
+'use script'
+
+function showMultiplicationTable(num){
+
+}
+
+// function showMultiplicationTable(){
+// var multiplier = 7;
+// for(i = 1; i <= 10; i++ ) {
+//     var result = multiplier * i;
+//     console.log(multiplier + 'X' + i + '=' + result);
+//     }
+// }
+// console.log(showMultiplicationTable());
+// var multiplier = 7;
+// for(i = 1; i <= 10; i++ ) {
+//     var result = multiplier * i;
+//     console.log(multiplier + '*' + i + '=' + result);}
+
+for(i = Math.random(); i < 200; i++){
+    console.log(i);
+}
+
+
