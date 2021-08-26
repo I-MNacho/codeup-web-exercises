66a87ed99b702fd5873da25eee07afab0326e128
Ignacio Mares <ignacio.mares00@gmail.com>
2021-08-26 16:51:45 -0500

    added walk thru and bonus

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -44,49 +44,81 @@ const threeLanguages = users.filter(user => user.languages.length  >= 3 );
 
 //3.Use .map to create an array of strings where each element is a user's email address
 
-let emailAddresses = users.map(item => item.email)
-// console.log(emailAddresses)
+let emailAddresses = users.map(user => user.email)
+console.log(emailAddresses)
 
 //4.Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
-let yearsOfExperience = users.reduce((total, users, index, array) => {
-    return total + users.yearsOfExperience / array.length
-}, 0)
+let yearsOfExperience = users.reduce((totalYears, users, index, array) =>
+    totalYears + users.yearsOfExperience / array.length, 0);
 console.log(yearsOfExperience)
 
 //5.Use .reduce to get the longest email from the list of users.
-let longestEmail = users.reduce((total, users, index, array) =>{
-if (index === array.length -1){
-    total.push(users.email)
-    total.sort((a,b) => b.length - a.length)
-    return total[0]
-}else{
-    total.push(users.email)
-    return total
-}
-}, []);
-console.log(longestEmail)
+//my solution
+// let longestEmail = users.reduce((total, users, index, array) =>{
+// if (index === array.length -1){
+//     total.push(users.email)
+//     total.sort((a,b) => b.length - a.length)
+//     return total[0]
+// }else{
+//     total.push(users.email)
+//     return total
+// }
+// }, []);
+// console.log(longestEmail)
+
+
+//Paris Shirley's solution:
+let longestEmail = users.reduce((a,b) =>{
+    if (a.length < b.email.length){
+        a = b.email
+    }
+    return a;
+}, '')
+console.log(longestEmail);
+
+//trying to one line Paris's solution
+// let longestEmail = users.reduce(((a, b) => a.length < b.email.length ? a = b.email : a), '')
+
+
 
 
 //6.Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
-let everyonesName = users.reduce((total, users, index, array) =>{
+let everyonesName = users.reduce((accumulator, users, index, array) =>{
     if (index === array.length -1){
-        total.push(users.name)
-        // total.sort((a,b) => b.length - a.length)
-        return `Your instructors are: ${total.join(', ')}`
+        accumulator.push(users.name)
+        return `Your instructors are: ${accumulator.join(', ')}`
     }else{
-        total.push(users.name)
-        return total
+        accumulator.push(users.name)
+        return accumulator
     }
 }, []);
 
 console.log(everyonesName)
 
+//BONUS:
+//1.Use .reduce to get the unique list of languages from the list of users.
+
+let uniqueUserLanguages = users.reduce((accumulator, user) => {
+let languages = user.languages;
+for (let i =0; i < languages.length; i++){
+    if (!accumulator.includes(languages[i])){
+        accumulator.push(languages[i]);
+    }
+}
+return accumulator.sort();
+}, []);
+console.log(uniqueUserLanguages);
 
 
 
-// if(index === array.length -1){
-//     names.push(users.name)
-//     return `Your instructors are ${names.join(', ')}`
+
+
+
+
+
+
+
+
 
 
 
