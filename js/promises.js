b9cefff6071cafd6c0818ba30db14c2c426d033b
Ignacio Mares <ignacio.mares00@gmail.com>
2021-08-30 14:25:17 -0500

    added promises solutions

--- js/promises.js
+++ js/promises.js
@@ -1,1 +1,13 @@
-fetch(url, {headers: {'Authorization': GIT_HUB_TOKEN}})
+// fetch('https://api.github.com/users', {headers: {'Authorization': GIT_HUB_TOKEN}})
+
+// `https://api.github.com/users/${username}/events`
+
+function lastPush(username) {
+    return fetch(`https://api.github.com/users/${username}/events`,
+    {headers: {'Authorization': 'token' + GIT_HUB_TOKEN}})
+        .then(response => response.json())
+        .then(response => response[0].created_at)
+        .catch(console.error)
+}
+
+console.log(lastPush('I-MNacho'))
